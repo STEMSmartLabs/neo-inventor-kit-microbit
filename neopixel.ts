d336eac3bed33f4126f0b7ca0253a924a5e79f05
Parthasarathy <mailtosarathy@gmail.com>
2025-12-11 15:11:56 +0530

    Renoved invalid object

--- neopixel.ts
+++ neopixel.ts
@@ -168,7 +168,7 @@
                     for (let i = 0; i < n; ++i) {
                         if (i <= v) {
                             const b = Math.idiv(i * 255, n1);
-                            this.setPixelColor(i, neopixel.rgb(b, 0, 255 - b));
+                            this.setPixelColor(i, rgb(b, 0, 255 - b));
                         }
                         else this.setPixelColor(i, 0);
                     }
